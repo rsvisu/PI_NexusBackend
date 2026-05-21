b114797409b9a51cc98baf2fc39a84436cbc78c3
Robert Sebastian Visu <robert.sebastian.visu@gmail.com>
2026-05-21 14:46:23 +0200

    feat: añadir gestion de errores de zod en  errorHandler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,17 +1,33 @@
-import consola from 'consola';
-import AppError from '../errors/AppError.js';
+import consola from 'consola'
+import { ZodError } from 'zod'
+import AppError from '../errors/AppError.js'
 
-function errorHandler(err, req, res, next) {
+function errorHandler(err, req, res, _next) {
     // Error esperado: lo lanzamos nosotros con AppError
     if (err instanceof AppError) {
-        return res.status(err.statusCode).json({ error: err.message });
+        return res.status(err.statusCode).json({ error: err.message })
+    }
+
+    // Error de validación de Zod: lo traduciomos a error 400 y mandamos el mensaje con solo el primer issue
+    if (err instanceof ZodError) {
+        const firstIssue = err.issues[0]
+        const path = firstIssue.path.join('.')
+        // Mensaje:
+        let message
+        if (path) {
+            message = `${path}: ${firstIssue.message}`
+        } else {
+            message = firstIssue.message
+        }
+        // Return:
+        return res.status(400).json({ error: message })
     }
 
     // Error inesperado: fallo de base de datos, bug, etc
-    consola.error(err);
+    consola.error(err)
     return res.status(500).json({
         error: "Lo siento, tengo problemas de conexión ahora mismo"
-    });
+    })
 }
 
-export default errorHandler;
+export default errorHandler
